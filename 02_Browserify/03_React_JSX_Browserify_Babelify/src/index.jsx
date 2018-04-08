ed31aca829e1f621a7f5e9923a1e45438e9c39f2
standard-software <standard.software.net@gmail.com>
2018-04-08 18:41:55 +0900

    フォルダ構成変更、reactify で import 文が使えなかったので、babelifyに変更した

---
+++ 02_Browserify/03_React_JSX_Browserify_Babelify/src/index.jsx
@@ -0,0 +1,14 @@
+// const React = require('react');
+// const ReactDOM = require('react-dom');
+import React from "react";
+import ReactDOM from "react-dom";
+
+class HelloWorld extends React.Component {
+  render() {
+    return <div>Hello, {this.props.name}</div>;
+  }
+}
+
+ReactDOM.render(
+  <HelloWorld name="World!" />,
+  document.getElementById('root'));
