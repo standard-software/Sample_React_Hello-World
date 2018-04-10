3dc809f6aa51ceb0f2fd83d93b8c11e583e6cd46
standard-software <standard.software.net@gmail.com>
2018-04-10 23:22:23 +0900

    よりシンプルにしたWebPackビルドのサンプル

---
+++ 03_WebPack/03_React_JSX_WebPack_MoreSimple/src/index.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class HelloWorld extends React.Component {
+  render() {
+    return <div>Hello, {this.props.name}</div>;
+  }
+}
+
+ReactDOM.render(
+  <HelloWorld name="World!" />,
+  document.getElementById('root'));
---
+++ 03_WebPack/03_React_JSX_WebPack_MoreSimple/webpack.config.js
@@ -0,0 +1,28 @@
+﻿const path = require('path');
+
+const src  = path.resolve(__dirname, 'src')
+const dist = path.resolve(__dirname, 'build')
+
+module.exports = {
+  entry: src + '/index.jsx',
+
+  output: {
+    path: dist,
+    filename: 'build.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+
+  plugins: []
+}
